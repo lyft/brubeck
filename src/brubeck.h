5b3f9f63a4c7501f1dd9b4c75ae532fd4ad0dd6c
Yash <yash@lyft.com>
2015-09-12 11:08:49 -0700

    deps

--- src/brubeck.h
+++ src/brubeck.h
@@ -29,8 +29,12 @@ typedef uint64_t hash_t;
 struct brubeck_server;
 struct brubeck_metric;
 
+#include "event2/event.h"
 #include "event2/util.h"
 #include "event2/thread.h"
+#include "event2/buffer.h"
+#include "event2/bufferevent.h"
+#include "event2/dns.h"
 #include "jansson.h"
 #include "log.h"
 #include "utils.h"
